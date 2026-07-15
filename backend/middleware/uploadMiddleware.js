156e930d51542b51d1835f1e12c60775943b83de
duynguyen1410 <nguyendinhduy1410@gmail.com>
2026-07-15 22:58:38 +0700

    fix: read .pdf

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -8,6 +8,13 @@ const storage = new CloudinaryStorage({
         folder: 'recruitment/cv',
         allowed_formats: ['pdf'],
         resource_type: 'raw',
+        access_mode: 'public',
+        format: 'pdf', // ép định dạng
+        public_id: (req, file) => {
+            // đảm bảo tên file có đuôi .pdf
+            const name = file.originalname.replace(/\.pdf$/i, '');
+            return '${ Date.now() }.pdf';
+        },
     },
 })
 
